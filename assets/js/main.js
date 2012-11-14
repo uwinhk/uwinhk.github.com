a664438033acd6794b26610ca73ff87fb82a21d1
aimeesn <aimee.sohn@gmail.com>
2012-11-14 10:22:11 +0800

    update thai

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,23 @@
 $(function() {
   console.log('[uwin.js]');
+
+  $("#main-nav li a.main-link").hover(function(){
+      $("#main-nav li a.close").fadeIn();
+      $("#main-nav li a.main-link").removeClass("active");
+      $(this).addClass("active");
+      $("#sub-link-bar").animate({
+          height: "70px"
+      });
+      $(".sub-links").hide();
+      $(this).siblings(".sub-links").fadeIn();
+  });
+  $("#main-nav li a.close").click(function(){
+      $("#main-nav li a.main-link").removeClass("active");
+      $(".sub-links").fadeOut();
+      $("#sub-link-bar").animate({
+          height: "30px"
+      });
+      $("#main-nav li a.close").fadeOut();
+  });
+
 });
